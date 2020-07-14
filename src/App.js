178eb8ab9ccc07d98c9bb0003fb6a383d8fcca44
Fahad Rehman <fahad.rehman365@yahoo.com>
2020-07-14 16:05:58 +0500

    Emojis For use Reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import SlotM from './SlotMachine'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  
+  return(
+    <div>
+
+       <h1 className="heading_style"> 🎰 Welcome TO{" "} <span style={{ fontWeight: 'bold' }}> Slot Machine Game </span> 🎰</h1>
+   
+<div className="">
+<SlotM x ="😃" y= "😃" z= "😃" />
+  <hr/>
+  <SlotM x ="😺" y= "😏" z= " 🤷‍♂" />
+  <hr/>
+  <SlotM x =" 👍 " y= " 🍌 " z= " 🍓" />
+  <hr/>
+  <SlotM x ="💯" y= "💯" z= "💯" />
+  <hr/>
+</div>
+
+       </div>
+  )
 }
-
 export default App;
